528edf19d587e2e4e2c281b8437bec48369a950a
Varaprasad-22 <virupavaraprasad22@gmail.com>
2025-12-31 13:35:14 +0530

    added the ticket service with proper impl

--- ticket-service/src/main/java/com/ticker_service/service/TicketServiceImpl.java
+++ ticket-service/src/main/java/com/ticker_service/service/TicketServiceImpl.java
@@ -3,17 +3,21 @@ package com.ticker_service.service;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ticker_service.dto.CreateTicketRequest;
 import com.ticker_service.model.Ticket;
+import com.ticker_service.repository.TicketRepository;
 
 import jakarta.validation.Valid;
 
 @Service
 public class TicketServiceImpl implements TickerService{
 
+	@Autowired
+	private TicketRepository ticketRepository;
 	@Override
 	public String createTicket(@Valid CreateTicketRequest request, List<MultipartFile> files, String userId) {
 		Ticket ticket=new Ticket();
@@ -25,6 +29,8 @@ public class TicketServiceImpl implements TickerService{
         ticket.setCreatedByUserId(userId);
         ticket.setCreatedAt(LocalDateTime.now());
         ticket.setUpdatedAt(LocalDateTime.now());
+        Ticket saved = ticketRepository.save(ticket);
+        
 		return null;
 	}
 
