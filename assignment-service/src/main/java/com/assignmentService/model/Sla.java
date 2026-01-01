1de618baf62159125719e038bc85cd932cfad7a0
Varaprasad-22 <virupavaraprasad22@gmail.com>
2026-01-01 20:49:20 +0530

    fixes

--- assignment-service/src/main/java/com/assignmentService/model/Sla.java
+++ assignment-service/src/main/java/com/assignmentService/model/Sla.java
@@ -2,11 +2,12 @@ package com.assignmentService.model;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.Id;
+
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
--- assignment-service/src/main/java/com/assignmentService/model/SlaRule.java
+++ assignment-service/src/main/java/com/assignmentService/model/SlaRule.java
@@ -1,13 +1,12 @@
 package com.assignmentService.model;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
