5247b450cd24103744c6ae68966411a25cc63934
Varaprasad-22 <virupavaraprasad22@gmail.com>
2026-01-03 02:22:34 +0530

    added the config server

---
+++ ticket-service/src/test/java/com/ticker_service/controller/TicketControllerTest.java
@@ -0,0 +1,22 @@
+//package com.ticker_service.controller;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.ticker_service.service.TickerService;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//@WebMvcTest(TicketController.class)
+//class TicketControllerTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @MockBean
+//    private TickerService ticketService;
+//
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//}
