3deb0da80fb61ad308cf4404b0477c9e175ce740
Varaprasad-22 <virupavaraprasad22@gmail.com>
2026-01-06 16:16:48 +0530

    increase code coveage

---
+++ ticket-service/src/test/java/com/ticker_service/service/NotificationPublisherTest.java
@@ -0,0 +1,40 @@
+package com.ticker_service.service;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import com.ticker_service.config.RabbitConfig;
+import com.ticker_service.dto.NotificationEvent;
+
+class NotificationPublisherTest {
+
+	@Mock
+	private RabbitTemplate rabbitTemplate;
+
+	@InjectMocks
+	private NotificationPublisher notificationPublisher;
+
+	@BeforeEach
+	void setup() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void publish_success() {
+
+		NotificationEvent event = new NotificationEvent("TICKET_CREATED", "user@test.com", "Ticket Created",
+				"Ticket created successfully");
+
+		String routingKey = "ticket.created";
+
+		notificationPublisher.publish(event, routingKey);
+
+		verify(rabbitTemplate).convertAndSend(RabbitConfig.Exchange, routingKey, event);
+	}
+}
